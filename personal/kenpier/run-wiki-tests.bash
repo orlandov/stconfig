8e745b7e8abf6b2b61b90bc22caaa254bb23077c
kenpier <kenpier@c8a2641f-bf1a-0410-9bac-bc2d7d3cadf3>
2007-08-05 20:19:40 +0000

    update run-wiki-tests path
    git-svn-id: https://repo.socialtext.net:8999/svn/stconfig/trunk@391 c8a2641f-bf1a-0410-9bac-bc2d7d3cadf3

--- personal/kenpier/run-wiki-tests.bash
+++ personal/kenpier/run-wiki-tests.bash
@@ -4,6 +4,9 @@ echo Retrieving test data
 cd ~/src/st/current/nlw
 dev-bin/create-test-data-workspace
 
+# ?? I don't think you should be using this.
+# nlw/dev-bin/link-control-panel
+# Fix this later
 /home/kenpier/import-control $2
 
 cd ~/src/st/current
@@ -11,5 +14,5 @@ cd ~/src/st/current
 nlw/dev-bin/nlwctl restart
 
 echo plan-page is $1
-~/src/stconfig/stbin/run-wiki-tests --timeout 30000 --plan-page "$1" >& testcases.out&
+~/stbin/run-wiki-tests --timeout 30000 --plan-page "$1" >& testcases.out&
 echo RUNNING ...
