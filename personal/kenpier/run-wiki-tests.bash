fee2520a9c5ecc7743e317c567e9ab593d275bae
kenpier <kenpier@c8a2641f-bf1a-0410-9bac-bc2d7d3cadf3>
2007-08-12 23:47:42 +0000

    Removed dependency on /home/kenpier/import-control
    git-svn-id: https://repo.socialtext.net:8999/svn/stconfig/trunk@410 c8a2641f-bf1a-0410-9bac-bc2d7d3cadf3

--- personal/kenpier/run-wiki-tests.bash
+++ personal/kenpier/run-wiki-tests.bash
@@ -1,18 +1,21 @@
 #!/bin/bash -e
 
 echo Retrieving test data
+~/src/st/current/nlw/dev-bin/create-test-data-workspace
+
+# check out the control repository
+echo Importing Control from trunk
+cd ~/src/st/
+svn co https://repo.socialtext.net:8999/svn/control
+
+# link it in the right place in the working copy
 cd ~/src/st/current/nlw
-dev-bin/create-test-data-workspace
+~/src/st/current/nlw/dev-bin/link-control-panel
 
-# ?? I don't think you should be using this.
-# nlw/dev-bin/link-control-panel
-# Fix this later
-/home/kenpier/import-control $2
+echo Restarting server for control to take effect
+~/src/st/current/nlw/dev-bin/nlwctl restart
 
-cd ~/src/st/current
-
-nlw/dev-bin/nlwctl restart
-
+cd ~/src/st/current/
 echo plan-page is $1
 ~/stbin/run-wiki-tests --timeout 30000 --plan-page "$1" >& testcases.out&
 echo RUNNING ...
