ed7d3247fc5a0ea77b49c13ec6f2ffb0824521ae
kenpier <kenpier@c8a2641f-bf1a-0410-9bac-bc2d7d3cadf3>
2008-03-18 16:39:55 +0000

    Replaced reference to directory kenpier with directory qa.  Replaced literal use of ~/src/st with
    git-svn-id: https://repo.socialtext.net:8999/svn/stconfig/trunk@548 c8a2641f-bf1a-0410-9bac-bc2d7d3cadf3

--- personal/qa/run-wiki-tests.bash
+++ personal/qa/run-wiki-tests.bash
@@ -16,57 +16,57 @@ then
 fi
 
 echo Removing all ceqlotron tasks to stop unnecessary indexing
-~/src/st/current/nlw/bin/ceq-rm /.+/
+$SRC_ST_BASE/current/nlw/bin/ceq-rm /.+/
 
 echo Retrieving test data
-~/src/st/current/nlw/dev-bin/create-test-data-workspace
+$SRC_ST_BASE/current/nlw/dev-bin/create-test-data-workspace
 
 if [ "$BRANCH" != "" ]
 then
     # check out control
     echo Importing Control from $BRANCH
-    cd ~/src/st/control/
+    cd $SRC_ST_BASE/control/
     svn co https://repo.socialtext.net:8999/svn/control/$BRANCH_PATH $BRANCH_PATH
     
     # link it in the right place in the working copy
-    cd ~/src/st/current/nlw
-    ~/src/st/current/nlw/dev-bin/link-control-panel $BRANCH
+    cd $SRC_ST_BASE/current/nlw
+    $SRC_ST_BASE/current/nlw/dev-bin/link-control-panel $BRANCH
     
     # check out console
     echo Importing Console from $BRANCH
-    cd ~/src/st/appliance/
+    cd $SRC_ST_BASE/appliance/
     svn co https://repo.socialtext.net:8999/svn/appliance/$BRANCH_PATH $BRANCH_PATH
     
     # link it in the right place in the working copy
-    cd ~/src/st/current/nlw
-    ~/src/st/current/nlw/dev-bin/link-console  $BRANCH
+    cd $SRC_ST_BASE/current/nlw
+    $SRC_ST_BASE/current/nlw/dev-bin/link-console  $BRANCH
     
     # check out reports
     echo Importing Reports from $BRANCH
-    cd ~/src/st/socialtext-reports/
+    cd $SRC_ST_BASE/socialtext-reports/
     svn co https://repo.socialtext.net:8999/svn/socialtext-reports/$BRANCH_PATH $BRANCH_PATH
     
     # link it in the right place in the working copy
-    cd ~/src/st/current/nlw
-    ##### ~/src/st/socialtext-reports/$BRANCH_PATH/setup-dev-env
+    cd $SRC_ST_BASE/current/nlw
+    ##### $SRC_ST_BASE/socialtext-reports/$BRANCH_PATH/setup-dev-env
     
     # check out skins
     echo Importing Socialtext Skins from $BRANCH
-    cd ~/src/st/socialtext-skins/
+    cd $SRC_ST_BASE/socialtext-skins/
     svn co https://repo.socialtext.net:8999/svn/socialtext-skins/$BRANCH_PATH $BRANCH_PATH
     
     # link it in the right place in the working copy
-    cd ~/src/st/current/nlw
-    ~/src/st/current/nlw/dev-bin/create-skinlink $BRANCH
+    cd $SRC_ST_BASE/current/nlw
+    $SRC_ST_BASE/current/nlw/dev-bin/create-skinlink $BRANCH
     
 fi
 
 echo Restarting apache server for control to take effect
-~/src/st/current/nlw/dev-bin/nlwctl  start
+$SRC_ST_BASE/current/nlw/dev-bin/nlwctl  start
 
-cd ~/src/st/current/
+cd $SRC_ST_BASE/current/
 echo plan-page is $1
 echo plan-workspace is $PLAN_WORKSPACE
 
 ~/stbin/run-wiki-tests --plan-workspace "$PLAN_WORKSPACE"  --timeout 60000 --plan-page "$1" >& testcases.out&
-echo RUNNING ...
+echo RUNNING ... tail testcases.out to monitor progress
