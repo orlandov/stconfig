3e64ff6220db3c8ee94e87d0738c95392ed14ee2
kenpier <kenpier@c8a2641f-bf1a-0410-9bac-bc2d7d3cadf3>
2008-04-10 23:16:39 +0000

    Better parameterization and cleaned up
    git-svn-id: https://repo.socialtext.net:8999/svn/stconfig/trunk@563 c8a2641f-bf1a-0410-9bac-bc2d7d3cadf3

--- personal/qa/run-local-wiki-tests.bash
+++ personal/qa/run-local-wiki-tests.bash
@@ -1,21 +1,26 @@
 #!/bin/bash -e
 
 PORT=`perl -e 'print $> + 20000'`
-SERVER=http://`hostname`:$PORT
-
+PLAN_SERVER=http://`hostname`:$PORT
 PLAN_WORKSPACE="wikitests"
-PLAN_SERVER=$SERVER
 
-if $ST_CURRENT/nlw/bin/st-admin create-user --e wikitester@ken.socialtext.net >/dev/null 2>/dev/null; then
+USERNAME="wikitester@ken.socialtext.net"
+USEREMAIL=$USERNAME
+
+if $ST_CURRENT/nlw/bin/st-admin create-user --e $USERNAME  >/dev/null 2>/dev/null; then
       echo "created wiki tester user"
 fi
 
-if  $ST_CURRENT/nlw/bin/st-admin add-workspace-admin --w test-data --e wikitester@ken.socialtext.net >/dev/null 2>/dev/null; then
+if  $ST_CURRENT/nlw/bin/st-admin add-workspace-admin --w test-data --e $USERNAME  >/dev/null 2>/dev/null; then
      echo "added workspace admin"
+fi
+
+if  $ST_CURRENT/nlw/bin/st-admin give-accounts-admin  --e $USERNAME  >/dev/null 2>/dev/null; then
+     echo "rave accounts admin"
 fi
 
 echo
 echo "running $1 from $PLAN_SERVER/$PLAN_WORKSPACE"
 echo
-$ST_SRC_BASE/stconfig/stbin/run-wiki-tests --no-maximize --plan-server "$PLAN_SERVER"  --plan-workspace "$PLAN_WORKSPACE"  --timeout 60000 --plan-page "$1"
+$ST_SRC_BASE/stconfig/stbin/run-wiki-tests --no-maximize --test-username $USERNAME --test-email $USEREMAIL --plan-server "$PLAN_SERVER"  --plan-workspace "$PLAN_WORKSPACE"  --timeout 60000 --plan-page "$1"
 
