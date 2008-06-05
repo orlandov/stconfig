4ea12e7c6ac9d082d2ad1128080a1124027996c8
kenpier <kenpier@c8a2641f-bf1a-0410-9bac-bc2d7d3cadf3>
2008-06-05 19:35:29 +0000

    Better conditionals to avoid spurious error msgs
    git-svn-id: https://repo.socialtext.net:8999/svn/stconfig/trunk@622 c8a2641f-bf1a-0410-9bac-bc2d7d3cadf3

--- personal/qa/tests-from-tarballs.bash
+++ personal/qa/tests-from-tarballs.bash
@@ -1,15 +1,22 @@
 #!/bin/bash -e
+
     if [ -e $ST_CURRENT/nlw/share/workspaces/wikitests/wikitests.1.tar.gz ]; then
+        if [ -e ~/.nlw/root/data/wikitests ]; then
+            echo DELETING existing wikitests workspace
+            $ST_CURRENT/nlw/bin/st-admin delete-workspace --w wikitests --no-export || true
+        fi    
         echo IMPORTING wikitests.1.tar.gz
-        $ST_CURRENT/nlw/bin/st-admin delete-workspace --w wikitests --no-export || true
         $ST_CURRENT/nlw/bin/st-admin import-workspace --t  $ST_CURRENT/nlw/share/workspaces/wikitests/wikitests.1.tar.gz
     else
         echo NO wikitests tarball to import
     fi
     
     if [ -e $ST_CURRENT/nlw/share/workspaces/calctests/calctests.1.tar.gz ]; then
+        if [ -e ~/.nlw/root/data/calctests ]; then
+            echo DELETING existing calctests workspace
+            $ST_CURRENT/nlw/bin/st-admin delete-workspace --w calctests --no-export || true
+        fi
         echo IMPORTING calctests.1.tar.gz
-        $ST_CURRENT/nlw/bin/st-admin delete-workspace --w calctests --no-export || true
         $ST_CURRENT/nlw/bin/st-admin import-workspace --t  $ST_CURRENT/nlw/share/workspaces/calctests/calctests.1.tar.gz
     else
         echo NO calctests tarball to import
