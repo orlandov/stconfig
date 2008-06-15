a93abea30b76d24716c069a723c284aa56bec15c
kenpier <kenpier@c8a2641f-bf1a-0410-9bac-bc2d7d3cadf3>
2008-06-15 07:40:51 +0000

    Refactored, simplified, more conditionals to reduce redundant processing
    git-svn-id: https://repo.socialtext.net:8999/svn/stconfig/trunk@636 c8a2641f-bf1a-0410-9bac-bc2d7d3cadf3

--- personal/qa/run-wiki-tests.bash
+++ personal/qa/run-wiki-tests.bash
@@ -1,139 +1,90 @@
 #!/bin/bash -e
 
-if [ "$1" == "" ]; then
+NLW_BIN="$ST_CURRENT/nlw/bin"
+NLW_DEVBIN="$ST_CURRENT/nlw/dev-bin"
+
+ if [ ! "$1" ]; then
     echo ""
     echo "Usage: run-wiki-tests.bash  PLAN_PAGE [BRANCH [1]]"
-    echo "means run plan page, optional [update BRANCH [fdefs if 1]]"
-    echo Wikitests are run from the local wikitests wiki
+    echo "Execute the wikitest plan page, optional [set and update BRANCH [fdefs if 1]]"
+    echo "Wikitests are run from the local wikitests wiki"
     echo "Export SELENIUM_PLAN_WORKSPACE and/or SELENIUM_PLAN_SERVER to run from external places"
     echo ""
     exit
 fi
 
+FRESHDEV=""
+if [ ! -e ~/.nlw ] || [ "$3" ] ; then  
+    FRESHDEV="yes"
+fi
+
 BRANCH=""
 BRANCH_PATH=""
+[ $2 ] && [ "$2" == "trunk" ] && BRANCH="trunk" && BRANCH_PATH="trunk"
+[ $2 ] && [ "$2" != "trunk" ] && BRANCH="$2" && BRANCH_PATH="branches/$2"
 
 PORT=`perl -e 'print $> + 20000'`
 PLAN_SERVER=http://`hostname`:$PORT
 PLAN_WORKSPACE="wikitests"
+[ "$SELENIUM_PLAN_WORKSPACE" ] && PLAN_WORKSPACE="$SELENIUM_PLAN_WORKSPACE"
+[ "$SELENIUM_PLAN_SERVER" ] && PLAN_SERVER="$SELENIUM_PLAN_SERVER"
+
+if [ "$BRANCH" ]; then
+    echo "set-branch $BRANCH"
+    ~/stbin/set-branch $BRANCH
+    REPOS=`~/stbin/st-repo-list | sed 's/socialtext //'` # all repos except socialtext
+
+    for REPO in $REPOS; do
+        echo Checking out $REPO/$BRANCH ;
+        mkdir -p  $ST_SRC_BASE/$REPO ;
+        cd $ST_SRC_BASE/$REPO ;
+        svn co https://repo.socialtext.net:8999/svn/$REPO/$BRANCH_PATH $BRANCH_PATH ;
+    done;
+fi
+
 USERNAME="wikitester@ken.socialtext.net"
-
-if [ "$2" != "" ]; then
-    if [ "$2" == "trunk" ]; then
-        BRANCH="trunk"
-        BRANCH_PATH="trunk"
-    else
-        BRANCH="$2"
-        BRANCH_PATH="branches/$2"
-    fi
-fi
-
-if [ "$SELENIUM_PLAN_WORKSPACE" != "" ]; then
-    PLAN_WORKSPACE="$SELENIUM_PLAN_WORKSPACE"
-fi
-
-if [ "$SELENIUM_PLAN_SERVER" != "" ]; then
-    PLAN_SERVER="$SELENIUM_PLAN_SERVER"
-fi
-
-if [ "$BRANCH" != "" ]; then
-    # check out control
-    echo Importing Control from $BRANCH
-    mkdir -p  $ST_SRC_BASE/control
-    cd $ST_SRC_BASE/control/
-    svn co https://repo.socialtext.net:8999/svn/control/$BRANCH_PATH $BRANCH_PATH
-    
-    # check out console
-    echo Importing Console from $BRANCH
-    mkdir -p $ST_SRC_BASE/appliance
-    cd $ST_SRC_BASE/appliance/
-    svn co https://repo.socialtext.net:8999/svn/appliance/$BRANCH_PATH $BRANCH_PATH
-    
-    # check out reports
-    echo Importing Reports from $BRANCH
-    mkdir -p $ST_SRC_BASE/socialtext-reports
-    cd $ST_SRC_BASE/socialtext-reports/
-    svn co https://repo.socialtext.net:8999/svn/socialtext-reports/$BRANCH_PATH $BRANCH_PATH
-    
-    # check out skins
-    echo Importing Socialtext Skins from $BRANCH
-    mkdir -p $ST_SRC_BASE/socialtext-skins
-    cd $ST_SRC_BASE/socialtext-skins/
-    svn co https://repo.socialtext.net:8999/svn/socialtext-skins/$BRANCH_PATH $BRANCH_PATH
-
-    # check out guanxi
-    echo Importing guanxi from $BRANCH
-    mkdir -p $ST_SRC_BASE/guanxi
-    cd $ST_SRC_BASE/guanxi/
-    svn co https://repo.socialtext.net:8999/svn/guanxi/$BRANCH_PATH $BRANCH_PATH
-    
-    # check out plugins
-    echo Importing plugins from $BRANCH
-    mkdir -p $ST_SRC_BASE/plugins
-    cd $ST_SRC_BASE/plugins/
-    svn co https://repo.socialtext.net:8999/svn/plugins/$BRANCH_PATH $BRANCH_PATH
-fi
-
-if [ ! -e ~/.nlw  ] || [ "$3"  != "" ]; then
-    refresh-branch
-    $ST_SRC_BASE/current/nlw/dev-bin/fresh-dev-env-from-scratch
-    $ST_SRC_BASE/current/nlw/dev-bin/create-test-data-workspace
-    $ST_CURRENT/nlw/bin/st-admin create-user --e $USERNAME >/dev/null 2>/dev/null || true
-    $ST_CURRENT/nlw/bin/st-admin add-workspace-admin --w test-data --e $USERNAME >/dev/null 2>/dev/null || true
-    $ST_CURRENT/nlw/bin/st-admin give-accounts-admin  --e $USERNAME  >/dev/null 2>/dev/null || true
+if [ $FRESHDEV ]; then
+    $NLW_DEVBIN/fresh-dev-env-from-scratch
+    echo Removing all ceqlotron tasks to stop unnecessary indexing
+    $NLW_BIN/ceq-rm /.+/
+    $NLW_DEVBIN/create-test-data-workspace
+    echo Creating user $USERNAME
+    $NLW_BIN/st-admin create-user --e $USERNAME >/dev/null 2>/dev/null || true
+    $NLW_BIN/st-admin add-workspace-admin --w test-data --e $USERNAME >/dev/null 2>/dev/null || true
+    $NLW_BIN/st-admin give-accounts-admin  --e $USERNAME  >/dev/null 2>/dev/null || true
+    $NLW_BIN/st-admin give-system-admin  --e $USERNAME  >/dev/null 2>/dev/null || true
+    echo Populating reports DB
+    $NLW_DEVBIN/st-populate-reports-db
     
     echo ""
     read -p  "Build wikitests wiki from scratch? y/n " wikitest
     if [ "$wikitest" == "y" ]; then
-        $ST_CURRENT/nlw/dev-bin/st-setup-wikitests
-        $ST_CURRENT/nlw/dev-bin/st-populate-reports-db
+        $NLW_DEVBIN/wikitests-to-wiki
     else
         echo ""
         read -p  "Build wikitests wiki from tarball? y/n " wikitest
-        [ "$wikitest" == "y" ] && $ST_CURRENT/nlw/bin/st-admin import-workspace --tarball $ST_CURRENT/nlw/share/workspaces/wikitests/wikitests.1.tar.gz  
+        [ "$wikitest" == "y" ] && $NLW_BIN/st-admin import-workspace --tarball $ST_CURRENT/nlw/share/workspaces/wikitests/wikitests.1.tar.gz  
     fi
 
     echo ""
     read -p  "Build calctests wiki from scratch? y/n " wikitest
     if [ "$wikitest" == "y" ]; then
-        $ST_CURRENT/nlw/dev-bin/calctests-to-wiki
+        $NLW_DEVBIN/calctests-to-wiki
     else
         echo ""
         read -p  "Build calctests wiki from tarball? y/n " wikitest
-        [ "$wikitest" == "y" ] && $ST_CURRENT/nlw/bin/st-admin import-workspace --tarball $ST_CURRENT/nlw/share/workspaces/calctests/calctests.1.tar.gz  
+        [ "$wikitest" == "y" ] && $NLW_BIN/st-admin import-workspace --tarball $ST_CURRENT/nlw/share/workspaces/calctests/calctests.1.tar.gz  
     fi
 
-else
-    if [ "$BRANCH" != "trunk" ]; then
-        LINK=$BRANCH
-    else
-        LINK=""
-    fi
-
-    echo Linking additional modules ...
-    echo ""
-    cd $ST_CURRENT/nlw/
-    $ST_CURRENT/nlw/dev-bin/link-control-panel $LINK
-    echo ""
-    $ST_CURRENT/nlw/dev-bin/link-console  $LINK
-    echo ""
-    $ST_CURRENT/nlw/dev-bin/create-skinlink $LINK
-    echo ""
-    $ST_CURRENT/nlw/dev-bin/link-reports
-    echo ""
-    $ST_CURRENT/nlw/dev-bin/link-people
 fi
 
 echo Removing all ceqlotron tasks to stop unnecessary indexing
-$ST_SRC_BASE/current/nlw/bin/ceq-rm /.+/
-
-echo Restarting apache server for control to take effect
-$ST_CURRENT/nlw/dev-bin/nlwctl  start
+$NLW_BIN/ceq-rm /.+/
 
 cd $ST_CURRENT
 echo plan-page is $1
 echo plan-workspace is $PLAN_WORKSPACE
 echo plan-server is $PLAN_SERVER
 
-$ST_SRC_BASE/stconfig/stbin/run-wiki-tests --no-maximize --test-username "$USERNAME" --test-email "$USERNAME" --plan-server "$PLAN_SERVER" --plan-workspace "$PLAN_WORKSPACE" --timeout 60000 --plan-page "$1" >& testcases.out&
-echo RUNNING ... tail -f testcases.out to monitor progress
+~/stbin/run-wiki-tests --no-maximize --test-username "$USERNAME" --test-email "$USERNAME" --plan-server "$PLAN_SERVER" --plan-workspace "$PLAN_WORKSPACE" --timeout 60000 --plan-page "$1" >& testcases.out&
+echo RUNNING ... tail -f $ST_CURRENT/testcases.out to monitor progress
